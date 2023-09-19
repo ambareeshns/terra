63d24f29cb9d198bf17883e15189edacc5b62712
ambareeshns <101534947+ambareeshns@users.noreply.github.com>
2023-09-19 22:31:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ provider "aws"{
 resource "aws_instance" "nam-ec2"{
   ami="ami-0dce6c913521d629e"
   instance_type="t2.micro"
+  user_data                   = <<-EOF
+      #!/bin/sh
+      sudo apt-get update
+      sudo apt install -y git
+      sudo apt install -y docker.io
+      sudo apt install -y maven
+EOF
   tags={
     Name="terraform"
   }
