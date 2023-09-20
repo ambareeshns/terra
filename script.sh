fd421f13fcf28be614bc389a20f31dbf4eb29cf7
ambareeshns <101534947+ambareeshns@users.noreply.github.com>
2023-09-20 22:53:01 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 git clone https://github.com/ambareeshns/work.git
 pwd
 cd work
-sudo mvn package
+mvn --version
+mvn package
 sleep 120
 sudo chmod 777 /var/run/docker.sock
 docker build -t namdu .
